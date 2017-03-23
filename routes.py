53a7f19d2548961db2d18362dbb0c6173935b83d
Taoge <wenter.wu@gmail.com>
2017-03-23 18:18:45 +0800

    + add logger

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import traceback
+import logging
 
 import ConfigParser
 
@@ -12,10 +13,12 @@ import json
 
 from flask import jsonify
 
+LOG = logging.getLogger(__name__)
+
 JSON_FILES_DIR = os.path.join(os.path.dirname(__file__), 'json_files')
 
 if os.path.exists(os.path.join(os.path.dirname(__file__), 'routes.cfg')):
-    print "READING ROUTES CONFIG "
+    LOG.info("Reading routes config")
     config = ConfigParser.ConfigParser()
     config.read('routes.cfg')
     _routes = config._sections.get("routes").items()
@@ -24,7 +27,9 @@ if os.path.exists(os.path.join(os.path.dirname(__file__), 'routes.cfg')):
         if not str(_r[0]).startswith('__'):
             routes.add(_r)
 
-    print "ROUTES :", routes
+    LOG.info("Loaded routes : %s", routes)
+
+
 else:
     routes = (
         ('/ping', 'ping.json'),
@@ -36,7 +41,12 @@ def gen_endpoint_func(json_file_name):
         json_file = os.path.join(JSON_FILES_DIR, json_file_name)
         assert os.path.exists(json_file), "json_file `%s` not exists" % json_file
         with open(os.path.join(JSON_FILES_DIR, json_file_name), 'r') as f:
-            return jsonify(json.load(f))
+            try:
+                response_data = jsonify(json.load(f))
+                return response_data
+            except Exception as e:
+                LOG.error("Fetch Json Error %s %s", json_file, str(e), exc_info=True)
+                return "Json File Load Error %s" % str(e)
 
     return f
 
@@ -52,7 +62,8 @@ def register_routes():
             app.add_url_rule(route[0], endpoint=gen_endpoint(route[0]), view_func=gen_endpoint_func(route[1]),
                              methods=['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE', 'PATCH'])
         except Exception as e:
-            print "Add route failure %s" % str(e)
+            LOG.warning("Add route failure %s", str(e))
             traceback.print_exc(file=sys.stdout)
 
     return app
+
